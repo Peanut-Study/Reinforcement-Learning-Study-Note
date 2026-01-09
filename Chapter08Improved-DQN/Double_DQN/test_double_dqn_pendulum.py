53365a9376949a4906b44017389af114df800ac4
Peanut-Study <xiaoyoung1001@outlook.com>
2026-01-09 19:54:50 +0800

    Add files via upload

---
+++ Chapter08Improved-DQN/Double_DQN/test_double_dqn_pendulum.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn.functional as F
+import gymnasium as gym
+import numpy as np
+#========================================================================
+env_name = 'Pendulum-v1'
+env = gym.make(env_name,render_mode="human")
+device = torch.device("cuda:0")
+#=========================================================================
+def dis_to_con(discrete_action, env, action_dim):  # 离散动作转回连续的函数
+    action_lowbound = env.action_space.low[0]      # 连续动作的最小值，获得环境的动作的界限
+    action_upbound = env.action_space.high[0]      # 连续动作的最大值
+    return action_lowbound + (discrete_action /(action_dim - 1)) * (action_upbound -action_lowbound)
+
+class Qnet(torch.nn.Module):
+    ''' 只有一层隐藏层的Q网络 '''
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(Qnet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))  # 隐藏层使用ReLU激活函数
+        return self.fc2(x)
+state_dim = env.observation_space.shape[0]
+hidden_dim = 128
+action_dim = 11  # 将连续动作分成11个离散动作
+q_net = Qnet(state_dim, hidden_dim, action_dim).to(device)
+q_net.load_state_dict(torch.load('double_dqn_pendulum_q_net.pth', map_location=device))
+q_net.eval()
+#===============================================================
+state, info = env.reset()
+done = False
+total_reward = 0
+while not done:
+    state_tensor = torch.tensor(np.array(state), dtype=torch.float32).to(device)
+    # 用训练好的模型选动作
+    with torch.no_grad():
+        action = q_net(state_tensor).argmax().item()
+    action = dis_to_con(action,env,action_dim)
+    # 执行动作
+    next_state, reward, terminated, truncated, info = env.step(np.array([action]))
+    done = terminated or truncated
+
+    state = next_state
+    total_reward += reward
+
+print("Total reward:", total_reward)
+env.close()
